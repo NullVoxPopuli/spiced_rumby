7f2fa01fcfb2d11d765b5bbaf1acede247a7962b
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-10-26 15:18:12 -0400

    chat is now scrollable

--- lib/spiced_rumby/gui/views/interfaces/chat.rb
+++ lib/spiced_rumby/gui/views/interfaces/chat.rb
@@ -11,10 +11,14 @@ Vedeu.interface :chat do
 
   keymap do
     key(:ctrl_c) { Vedeu.exit }
-    key(:tab){ Vedeu.focus_next }
-    key('t') { Vedeu.focus_by_name(:input) } # to chat
-    key('b') { } # to block
-    key('w') { } # to whisper
+    key(:tab)    { Vedeu.focus_next }
+    key(:up)     { Vedeu.trigger(:_cursor_up_)    }
+    key(:right)  { Vedeu.trigger(:_cursor_right_) }
+    key(:down)   { Vedeu.trigger(:_cursor_down_)  }
+    key(:left)   { Vedeu.trigger(:_cursor_left_)  }
+    key('t')     { Vedeu.focus_by_name(:input) } # to chat
+    key('b')     { } # to block
+    key('w')     { } # to whisper
   end
 
   group :main
